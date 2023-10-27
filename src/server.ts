e599aa1a113fcb85066cc78a48d32a0d3146f8d6
Gusferreirac <gusferreiracustodio@gmail.com>
2023-10-27 00:16:06 -0300

    Server port included

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,8 @@ app.register(humorHandler)
 
 
 app.listen({
-  port: 3333
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT):  3333
 }).then(() => {
   console.log('HTTP Server Running!')
 })
